13aa875612bfae4013193fa7bb24948ba717afd2
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-02-18 00:41:05 -0500

    created mongo db schemas

---
+++ models/Employee.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const EmployeeSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: [true, 'Please enter first name'],
+    trim: true,
+    lowercase: true
+  },
+  lastname: {
+    type: String,
+    alias: 'surname',
+    required: true,
+    trim: true,
+    lowercase: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: [true, "Duplicate Email Not allowed"],
+    trim: true,
+    uppercase: true,
+    maxlength: 50,
+    //Custom validation
+    validate: function(value) {
+      var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+      return emailRegex.test(value);
+    }
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ['male', 'female', 'other'],
+    trim: true,
+    lowercase: true
+  },
+  salary: {
+    type: Number,
+    default: 0.0,
+    validate(value) {
+      if (value < 0.0){
+         throw new Error("Negative Salary aren't real.");
+      }
+    }
+  },
+  created: { 
+    type: Date,
+    default: Date.now,
+    alias: 'createdat'
+  },
+  updatedat: { 
+    type: Date,
+    default: Date.now
+  },
+});
+
+const Employee = mongoose.model("Employee", EmployeeSchema);
+module.exports = Employee;
---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Please enter username'],
+    trim: true,
+    lowercase: true,
+    unique: [true, 'User name already exists']
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: [true, "Duplicate Email Not allowed"],
+    trim: true,
+    uppercase: true,
+    maxlength: 50,
+    //Custom validation
+    validate: function(value) {
+      var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+      return emailRegex.test(value);
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  created: { 
+    type: Date,
+    default: Date.now,
+    alias: 'createdat'
+  },
+  updatedat: { 
+    type: Date,
+    default: Date.now
+  },
+});
+
+const User = mongoose.model("Employee", UserSchema);
+module.exports = User;
