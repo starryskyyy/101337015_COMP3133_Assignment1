2f96959cc8b255a47f7adb0d91413f1a967ea0cb
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-02-19 15:24:26 -0500

    finished apis

--- resolvers.js
+++ resolvers.js
@@ -1,24 +1,38 @@
 const Employee = require('./models/Employee');
 const User = require('./models/User');
 
+
 exports.resolvers = {
     Query: {
         getEmployees: async (parent, args) => {
             return Employee.find({})
         },
         getEmployeeByID: async (parent, args) => {
-            return Employee.findById(args.id)
+            try {
+                return await Employee.findById(args.id);
+
+            } catch (err) {
+                if (!args.id) {
+                    throw new Error(`ID field can not be empty`)
+                }
+                else if (err.name === 'CastError') {
+                    throw new Error(`Invalid employee id`)
+                }
+                throw new Error(`An error occurred while getting the employee`)
+            }
         },
+
         login: async (parent, args) => {
             try {
-                const user = await User.findOne({ username: args.username });
+                const user = await User.findOne({ username: args.username })
+
                 if (user && user.password === args.password) {
-                    return `User ${user.username} logged in successfully`;
+                    return `User ${user.username} logged in successfully`
                 } else {
-                    return "Invalid username or password";
+                    return "Invalid username or password"
                 }
             } catch (err) {
-                return err.message;
+                return JSON.stringify(err.message)
             }
         }
     },
@@ -31,22 +45,38 @@ exports.resolvers = {
                     lastname: args.lastname,
                     email: args.email,
                     gender: args.gender,
-                    salary: args.salary
+                    salary: args.salary,
                 });
-                return await newEmp.save();
+
+
+                return await newEmp.save()
+
             } catch (err) {
-                return JSON.stringify(err.message)
+                if (!args.firstname || !args.lastname || !args.email || !args.gender || !args.salary) {
+                    throw new Error(`Fields cannot be empty`)
+                }
+                if (err.code === 11000) {
+                    const duplicateKey = Object.keys(err.keyPattern)[0];
+                    throw new Error(`User with this ${duplicateKey} already exists`)
+                } else if (err.name === 'ValidationError') {
+                    if (err.errors.salary) {
+                        throw new Error('Salary cannot be negative')
+                    }
+                    throw new Error(`Invalid email format`)
+                } else {
+                    throw new Error('An error occurred while creating the user')
+                }
             }
         },
         updateEmployee: async (parent, args) => {
-            console.log(args)
-            try {
-                console.log(args)
-                if (!args.id) {
-                    return;
-                }
 
-                const employee = await Employee.findOneAndUpdate(
+            if (!args.firstname || !args.lastname || !args.email || !args.gender || !args.salary) {
+                throw new Error(`Fields cannot be empty`)
+            }
+
+            try {
+
+                return await Employee.findOneAndUpdate(
                     {
                         _id: args.id
                     },
@@ -59,43 +89,59 @@ exports.resolvers = {
                             salary: args.salary
                         }
                     }, { new: true }
-                );
+                )
 
-                return employee;
             } catch (err) {
-                return JSON.stringify(err.message);
+
+                if (!args.id || err.name === 'CastError') {
+                    throw new Error(`Employee does not exists`);
+                }
+                if (err.code === 11000) {
+                    const duplicateKey = Object.keys(err.keyPattern)[0];
+                    throw new Error(`Employee with this ${duplicateKey} already exists`)
+                } else {
+                    throw new Error('An error occurred while creating the user')
+                }
             }
         },
         deleteEmployee: async (parent, args) => {
-            console.log(args)
             if (!args.id) {
-                return JSON.stringify({ status: false, "message": "No ID found" });
+                throw new Error( 'ID can not be empty');
             }
-            return await Employee.findByIdAndDelete(args.id)
+            try{
+                return await Employee.findByIdAndDelete(args.id)
+            }
+            catch(err){
+                if (err.name === 'CastError') {
+                    throw new Error(`Employee does not exists`)
+                }
+                throw new Error(`An error occurred while getting the employee`)
+            }
         },
 
         signup: async (parent, args) => {
 
             try {
-                if (!args.username || !args.password || !args.email) {
-                    return { message: null, error: `Fields can not be empty` };
-                }
                 const user = new User({
                     username: args.username,
                     email: args.email,
                     password: args.password
-                });
-                await user.save();
-                return { message: 'User created successfully', error: null };
+                })
+
+                return await user.save()
             } catch (err) {
-                
-                if (err.code === 11000) {
+
+                if (!args.username || !args.password || !args.email) {
+                    throw new Error(`Fields can not be empty`)
+                }
+                else if (err.code === 11000) {
                     const duplicateKey = Object.keys(err.keyPattern)[0];
-                    return { message: `User with this ${duplicateKey} already exists`, error: null };
+                    throw new Error(`User with this ${duplicateKey} already exists`)
+
                 } else if (err.name === "ValidationError") {
-                    return { message: null, error: `Invalid email format` };
+                    throw new Error(`Invalid email format`)
                 } else {
-                    return { message: null, error: "An error occurred while creating the user" };
+                    throw new Error('An error occurred while creating the user')
                 }
             }
         }
